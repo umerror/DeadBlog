f69fc6dd47b49cf3c4fc7593258d95fbca6a2782
umerror <mumernawaz@gmail.com>
2016-05-06 13:14:42 +0500

    Add Validations to post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,20 @@ class PostsController < ApplicationController
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
 		@post = Post.new(post_params)
-		@post.save
+
+		if @post.save
 
 		redirect_to @post
+		else
+			render 'new'
+		end 
 	end
+
 	def show
 		@post = Post.find(params[:id])
 	end
